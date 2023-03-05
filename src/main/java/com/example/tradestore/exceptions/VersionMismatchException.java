fded0dfb4c9d965c341a90cd3fde430424e45e7f
amey <amey.sambhare@gmail.com>
2023-03-05 13:33:24 +0530

    Pushing changes to github

---
+++ src/main/java/com/example/tradestore/exceptions/VersionMismatchException.java
@@ -0,0 +1,7 @@
+package com.example.tradestore.exceptions;
+
+public class VersionMismatchException extends Exception{
+    public VersionMismatchException(String message) {
+        super(message);
+    }
+}
